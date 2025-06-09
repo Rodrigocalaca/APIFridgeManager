96c93797a96c56bed1cb4801e774389d73ed3fa8
Rodrigo Calaca <rodrigo.caetano@trinusco.com.br>
2025-06-09 12:34:06 -0300

    Aplicando levenshtein

--- src/main/java/com/rodrigodev/FridgeManager/Services/BaseProductService.java
+++ src/main/java/com/rodrigodev/FridgeManager/Services/BaseProductService.java
@@ -1,6 +1,10 @@
 package com.rodrigodev.FridgeManager.Services;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.text.similarity.LevenshteinDistance;
 
 import com.rodrigodev.FridgeManager.Models.Product;
 import com.rodrigodev.FridgeManager.Models.StorageType;
@@ -23,11 +27,26 @@ public abstract class BaseProductService implements ProductService {
 
     @Override
     public List<Product> listItems() {
-        return productRepository.findByStorageType(storageType);
+    List<Product> products = productRepository.findByStorageType(storageType);
+
+        return products.stream()
+            .sorted(Comparator.comparing(Product::getExpirationDate, Comparator.nullsLast(Comparator.naturalOrder())))
+            .collect(Collectors.toList());
     }
 
     @Override
     public void addProduct(Product product) {
+        List<Product> products = productRepository.findByStorageType(storageType);
+        LevenshteinDistance levenshtein = new LevenshteinDistance();
+
+        for (Product existingProduct : products) {
+            if (levenshtein.apply(existingProduct.getName().toLowerCase(), product.getName().toLowerCase()) <= 2) {
+                existingProduct.setQuantity(existingProduct.getQuantity() + product.getQuantity());
+                productRepository.save(existingProduct);
+                return; 
+            }
+        }
+
         product.setStorageType(storageType);
         productRepository.save(product);
     }
